9227b1b46df7855dd6612135f95e6a52297a1ce3
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2025-02-21 22:41:50 +0800

    Update lora_finetune_single_device.py

--- recipes/lora_finetune_single_device.py
+++ recipes/lora_finetune_single_device.py
@@ -784,7 +784,7 @@ class LoRAFinetuneRecipeSingleDevice(FTRecipeInterface):
                 self.epochs_run += 1
                 start_save_checkpoint = time.perf_counter()
                 log.info("Starting checkpoint save...")
-                self.save_checkpoint(epoch=curr_epoch)
+                # self.save_checkpoint(epoch=curr_epoch)
                 log.info(
                     "Checkpoint saved in {:.2f} seconds.".format(
                         time.perf_counter() - start_save_checkpoint
